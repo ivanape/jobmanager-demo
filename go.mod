ff21cc9b04c0785016b828adab7f81c9d04e8005
Iván Álvarez <ialvarezpereira@gmail.com>
2021-01-11 12:13:23 +0100

    Update example to latest API version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ivana/jobmanager-demo
 
 go 1.15
 
-require github.com/ivanape/jobmanager v0.0.0-20210110095018-3d8d56a6c333
+require github.com/ivanape/jobmanager v0.0.0-20210111110317-9c366ee09c2f
--- main.go
+++ main.go
@@ -3,28 +3,71 @@ package main
 import (
 	"fmt"
 	"github.com/ivanape/jobmanager"
+	"time"
 )
 
-var jobManager *jobmanager.JobsManager
+var manager *jobmanager.JobsManager
 
 func init() {
-	jobManager = jobmanager.NewJobManager(1)
+	manager = jobmanager.NewJobManager(1)
 }
 
 func main() {
-	job, err := jobmanager.NewJob(func() {
-		fmt.Println("Hello world!")
-	})
+	var job *jobmanager.Job
+	var err error
+
+	f := func(name string) (string, error) {
+		fmt.Printf("Hello %s!\n", name)
+		time.Sleep(1 * time.Second)
+
+		return "any value", nil
+	}
+
+	// Run job as function async
+	job, err = manager.Run(f, "Job")
+	if err != nil {
+		panic(err)
+	}
+	// Wait until job ends
+	manager.WaitForJobs(job)
+	fmt.Printf("Return value is: %s\n", job.Result.Value.(string))
+	fmt.Printf("Error value is: %v\n", job.Result.Err)
+
+	// Run and wait a function
+	job, err = manager.RunAndWait(f, "Job")
 	if err != nil {
 		panic(err)
 	}
 
-	jobManager.RunJobAndWait(job)
+	// Define a job as a function
+	job, err = manager.NewJob(f, "Job")
+	if err != nil {
+		panic(err)
+	}
+	// Same flow to execute
+	manager.RunJob(job)
+	manager.WaitForJobs(job)
+	manager.RunJob(job)
 
-	job2, err := jobManager.RunAndWait(func() { fmt.Println("Hello world!") })
+	// Batch job execution
+	job1, _ := manager.NewJob(f, "Job 1")
+	job2, _ := manager.NewJob(f, "Job 2")
+	// Run both jobs in Sequentially
+	manager.RunJobsInSerial(job1, job2)
+	// Run both jobs in Parallel
+	manager.RunJobsInParallel(job1, job2)
 
-	jobManager.RunJobsInSequence(job, job2)
-	jobManager.RunJobsInParallel(job, job2)
+	// Logical job aggregation
+	job3, _ := manager.NewJob(f, "Job 3")
+	job4, _ := manager.NewJob(f, "Job 4")
+	err = manager.CreateGroup("groupName", job3, job4)
 
-	fmt.Println(job2.Status)
+	// Run both jobs in Sequentially
+	manager.RunJobsInSerial(job3, job4)
+	// Run both jobs in Parallel
+	manager.RunJobsInParallel(job3, job4)
+	jobs, err := manager.GetJobsByGroup("groupName")
+	for _, j := range jobs {
+		fmt.Printf("Job is %s status is %d\n", j.ID, j.Status)
+	}
 }
