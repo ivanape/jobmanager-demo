6733a510c7c00606a837aa4fb2fcd8a3e6d57da4
Iván Álvarez <ialvarezpereira@gmail.com>
2021-01-11 12:51:38 +0100

    Update example to latest API version

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ivanape/jobmanager"
+	"log"
 	"time"
 )
 
 var manager *jobmanager.JobsManager
 
 func init() {
-	manager = jobmanager.NewJobManager(1)
+	manager = jobmanager.NewJobManager(10)
 }
 
 func main() {
@@ -17,7 +17,7 @@ func main() {
 	var err error
 
 	f := func(name string) (string, error) {
-		fmt.Printf("Hello %s!\n", name)
+		log.Printf("Hello %s!\n", name)
 		time.Sleep(1 * time.Second)
 
 		return "any value", nil
@@ -30,8 +30,8 @@ func main() {
 	}
 	// Wait until job ends
 	manager.WaitForJobs(job)
-	fmt.Printf("Return value is: %s\n", job.Result.Value.(string))
-	fmt.Printf("Error value is: %v\n", job.Result.Err)
+	log.Printf("Return value is: %s\n", job.Result.Value.(string))
+	log.Printf("Error value is: %v\n", job.Result.Err)
 
 	// Run and wait a function
 	job, err = manager.RunAndWait(f, "Job")
@@ -47,27 +47,25 @@ func main() {
 	// Same flow to execute
 	manager.RunJob(job)
 	manager.WaitForJobs(job)
-	manager.RunJob(job)
+	manager.RunJobAndWait(job)
 
 	// Batch job execution
 	job1, _ := manager.NewJob(f, "Job 1")
 	job2, _ := manager.NewJob(f, "Job 2")
 	// Run both jobs in Sequentially
 	manager.RunJobsInSerial(job1, job2)
-	// Run both jobs in Parallel
-	manager.RunJobsInParallel(job1, job2)
-
-	// Logical job aggregation
 	job3, _ := manager.NewJob(f, "Job 3")
 	job4, _ := manager.NewJob(f, "Job 4")
-	err = manager.CreateGroup("groupName", job3, job4)
-
-	// Run both jobs in Sequentially
-	manager.RunJobsInSerial(job3, job4)
 	// Run both jobs in Parallel
 	manager.RunJobsInParallel(job3, job4)
+
+	// Logical job aggregation
+	job5, _ := manager.NewJob(f, "Job 5")
+	job6, _ := manager.NewJob(f, "Job 6")
+	err = manager.CreateGroup("groupName", job5, job6)
+	manager.RunJobsInSerial(job5, job6)
 	jobs, err := manager.GetJobsByGroup("groupName")
 	for _, j := range jobs {
-		fmt.Printf("Job is %s status is %d\n", j.ID, j.Status)
+		log.Printf("Job is %s status is %d\n", j.ID, j.Status)
 	}
 }
